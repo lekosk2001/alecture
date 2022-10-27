fb7016b53af63ac07bbbe2177213db7c40d02572
lekosk2001 <lekosk2001@naver.com>
2022-10-27 23:58:00 +0900

    세팅

---
+++ layouts/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Routes } from "react-router-dom";
+import Login from '../pages/Login'
+import SignUp from '../pages/SignUp'
+
+export default function App() {
+    return <div>초기 세팅입니다.</div>
+}
---
+++ pages/SignUp/index.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function SignUp() {
+    return <div>초기 세팅입니다.</div>
+}
