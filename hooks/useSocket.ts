cd1311b486ab0255c17e8e03044b66e9ce9889a3
lekosk2001 <lekosk2001@naver.com>
2022-11-08 00:24:54 +0900

    feat : 소켓 기능.

--- hooks/useSocket.ts
+++ hooks/useSocket.ts
@@ -1,10 +1,26 @@
-import io from 'socket.io-client';
 import { useCallback } from 'react';
-import axios from 'axios';
+import io from 'socket.io-client';
 
 const backUrl = 'http://localhost:3095';
-const useSocket = () => {
-    const socket = io.connect(`${backUrl}`);
+
+const sockets:{[key:string]:SocketIOClient.Socket} = {};
+
+const useSocket = (workspace?:string):[SocketIOClient.Socket|undefined,()=>void] => {
+
+    const disconnect = useCallback(() => {
+        if(workspace){
+            sockets[workspace].disconnect();
+            delete sockets[workspace]
+        }
+    },[])
+    
+    if(!workspace){ return[undefined,disconnect] }
+
+    sockets[workspace] = io.connect(`${backUrl}/ws-${workspace}`);
+    sockets[workspace].emit('hello','world');
+    sockets[workspace].on('message',(data: any)=>{console.log(data);})
+    sockets[workspace].on('onLineList',(data: any)=>{console.log(data);})
+    return [sockets[workspace],disconnect];
 };
 
 export default useSocket;
